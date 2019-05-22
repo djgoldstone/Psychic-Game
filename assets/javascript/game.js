0a1022d17474729276f291aa07daf090addcec88
Derek Goldstone <derekgoldstone@yahoo.com>
2019-05-22 09:49:47 -0700

    Declared variables containg guessesLeft which will decrement and an empty array which will contain pushed event.key from user. Declared wins/loss/userguess/remainingguesses variables which will display on to HTML page. Declared document.onkeyup function which stores key event in userGuess variable and determines computerChoice by utilizing Math.floor (to round) and Math.random (to generate random number) multiplied by the length of the elements in the alphabet variable (to determine index size).

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,4 +2,16 @@ var alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
 
 var wins = 0;
 var losses = 0;
-var guesses = 10;
+var guessesLeft = 10;
+
+var guessedLetters = [];
+
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var userGuessesText = document.getElementById("user-guesses-text");
+var remainingGuessesText = document.getElementById("remaining-guesses-text");
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    var computerChoice = Math.floor(Math.random() * alphabet.length);
+}

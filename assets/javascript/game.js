1c46b4988ec62597abb77d0ff9b30471eede9229
Derek Goldstone <derekgoldstone@yahoo.com>
2019-05-23 08:48:24 -0700

    Added conditional statement checking whether keyup event equates to a letter by alertig brower if a character greater than or equal to zero or longer than one string character is not a letter.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphabet = "abcdefghojklmnopqrstuvwxyz";
 
 var wins = 0;
 var losses = 0;
@@ -15,7 +16,7 @@ var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 userGuesses.textContent = "You chose: ";
 winsText.textContent = "Wins: ";
 lossesText.textContent = "Losses: ";
-remainingGuessesText.textContent = "Guesses Remaining: ";
+remainingGuessesText.textContent = "Guesses Remaining: 10";
 userGuessesText.textContent = "Your guesses so far...";
 
 
@@ -24,8 +25,17 @@ document.onkeyup = function(event) {
     var userGuess = event.key.toLowerCase();
     guessedLetters.push(userGuess);
 
+    
+    if (userGuess >= 0 || userGuess.length > 1) { 
+        alert("Choose a letter!");
+    } else {
+            
+            
+        
+    
         if (userGuess === computerChoice) {
             wins++;
+            alert("Nice!");
         } else {
             guessesLeft--;
         };
@@ -42,8 +52,10 @@ document.onkeyup = function(event) {
     userGuessesText.textContent = "Your guesses so far..." + guessedLetters;
 
     
+        } 
     
-    };
+
+};
 
     
 
